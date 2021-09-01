ff091ca706379ec725966d3821d46d27305c8e35
shoestrong <qianchuang.zhang@baodanyun-inc.com>
2021-09-01 15:28:32 +0800

    feat: update url

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 3030
 
 module.exports = {
 	port: PORT,
-	downloadUrl: `${getIPAddress()}`,
+	downloadUrl: process.env.DEBUG === 'dev' ? `http://${getIPAddress()}:${PORT}` : `${getIPAddress()}`,
 	ipAddress: `http://${getIPAddress()}:${PORT}`
 };
